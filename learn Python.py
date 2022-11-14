67b2ec85d0535bda6ff14f1e53e20e7c7b8d8fb0
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-11-14 17:47:26 +0200

    Learn Python

---
+++ learn Python.py
@@ -0,0 +1,19 @@
+"""
+   if 5 > 2:
+    
+     print("Five is greater than two!")
+"""
+"""if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+  """
+
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar2)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+
